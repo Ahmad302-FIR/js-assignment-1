95c0a260bb1d26202eaa21dea8d75e7c101a1adf
Ahmad302-FIR <1ahmad2khan345@gmail.com>
2026-01-28 18:51:30 +0500

    Added

---
+++ script.js
@@ -0,0 +1,14 @@
+// Allert
+alert("Hello word");
+// Hello word
+console.log("Hello word");
+// Variables
+var studentName = "Muhammad Ahmed";
+let courseName = "Web Development";
+let batchNumber = 1;
+courseName="graphic desinging";
+const instituteName = "SMIT Peshawar";
+console.log(studentName);
+console.log(courseName);
+console.log(batchNumber);
+console.log(instituteName);
